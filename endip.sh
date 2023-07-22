cb4cd5acf9402dea42b3dff5f5392f21415c954b
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-07-23 00:35:28 +0330

    Update endip.sh

--- endip.sh
+++ endip.sh
@@ -98,49 +98,49 @@ endipv4(){
 		then
 			break
 		else
-			temp[$n]=$(echo 162.159.192.$(($RANDOM%256)))
+			temp[$n]=$(echo 104.31.16.$(($RANDOM%256)))
 			n=$[$n+1]
 		fi
 		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]
 		then
 			break
 		else
-			temp[$n]=$(echo 162.159.193.$(($RANDOM%256)))
+			temp[$n]=$(echo 104.31.16.$(($RANDOM%256)))
 			n=$[$n+1]
 		fi
 		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]
 		then
 			break
 		else
-			temp[$n]=$(echo 162.159.195.$(($RANDOM%256)))
+			temp[$n]=$(echo 104.31.16.$(($RANDOM%256)))
 			n=$[$n+1]
 		fi
 		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]
 		then
 			break
 		else
-			temp[$n]=$(echo 188.114.96.$(($RANDOM%256)))
+			temp[$n]=$(echo 104.31.16.$(($RANDOM%256)))
 			n=$[$n+1]
 		fi
 		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]
 		then
 			break
 		else
-			temp[$n]=$(echo 188.114.97.$(($RANDOM%256)))
+			temp[$n]=$(echo 104.31.16.$(($RANDOM%256)))
 			n=$[$n+1]
 		fi
 		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]
 		then
 			break
 		else
-			temp[$n]=$(echo 188.114.98.$(($RANDOM%256)))
+			temp[$n]=$(echo 104.31.16$(($RANDOM%256)))
 			n=$[$n+1]
 		fi
 		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]
 		then
 			break
 		else
-			temp[$n]=$(echo 188.114.99.$(($RANDOM%256)))
+			temp[$n]=$(echo 104.31.16.$(($RANDOM%256)))
 			n=$[$n+1]
 		fi
 	done
